f9403f51be3b8d6140faee79aa93bb624dc585f1
pauldubois98 <pauldubois98@gmail.com>
2023-08-15 11:04:28 +0200

    added classes folders

--- save_image.php
+++ save_image.php
@@ -1,10 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $imageData = $_POST["imageData"];
+    $imageType = $_POST["imageType"];
     $base64Data = str_replace("data:image/png;base64,", "", $imageData);
-    
-    $imagePath = "drawings/" . time() . ".png";
-    
+
+    $imagePath = $imageType . "s/" . time() . ".png";
+
     if (file_put_contents($imagePath, base64_decode($base64Data))) {
         echo json_encode(["success" => true]);
     } else {
